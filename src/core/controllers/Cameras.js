72cece49cb14b5c36fa18654b7061818e63bcf68
Franck <136079691+Freddyede@users.noreply.github.com>
2024-05-08 15:38:01 +0200

    Adding Cameras.js to the project for camera management

---
+++ src/core/controllers/Cameras.js
@@ -0,0 +1,9 @@
+/**
+ * This class is for the camera of player
+ * @version 0.0.1
+ * @author Patouillard Franck <patouillardfranck.development@gmail.com>
+ */
+export class Cameras {
+    constructor() {
+    }
+}
